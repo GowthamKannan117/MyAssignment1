c9a967f706e4a79867f66bcf3633b4a32db9d383
GowthamKannan117 <gowthamk.work@gmail.com>
2024-08-20 13:36:19 +0530

    Add files via upload
    Week1 Day1 Assignment

---
+++ week1Day1/Chrome.java
@@ -0,0 +1,21 @@
+package datatypes;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version= 91.0f;
+		String developer= "Google";
+		int releaseYear= 2008;
+		boolean isBeta= false;
+		char shortcutKey= 'C';
+		
+		System.out.println("Chrome Version:" + ' ' + version);
+		System.out.println("Search Engine:" + ' ' + developer);
+		System.out.println("Release Year:" + ' ' +releaseYear);
+		System.out.println("Is a Beta Version:" + ' ' +isBeta);
+		System.out.println("Shortcut" + ' ' +shortcutKey);
+	}
+
+}
